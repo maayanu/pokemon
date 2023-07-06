b81c30418042e2071e0be75dca02f3c12f540048
maayan uliel <maayanmaryuma@h-MacBook-Pro-sl-maayan.local>
2023-07-06 16:32:42 +0300

    first commit

---
+++ src/components/PokemonPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function PokemonPage() {
+  return (
+    <div>
+        <h1>Pokémon</h1>
+        <input></input>
+        <button>Search</button>
+    </div>
+  )
+}
+
+export default PokemonPage
